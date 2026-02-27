9991a55f7e29f5656dee67294948106ee352bc8c
ms4851-oss <ms4851@srmist.edu.in>
2026-02-27 15:26:06 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,15 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        System.out.println("Welcome to palindrome Checker app");
+        System.out.println("Palindrome Checker- Hardcore Input");
+        string word="madam";
+        string reverse="";
+        for(int i=word.length()-1;i>=0;i--){
+            reverse=reverse + word.charAt(i);
+        }
+        if(word.equals(reverse)){
+            System.out.println(word + "is a palindrome");
+        }else{
+            System.out.println(word + "is NOT a Palindrome");
+        }
     }
 }
