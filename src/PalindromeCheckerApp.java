ea0fbf5d5e7dca4d85bbfb478f2dce410f88e13b
ms4851-oss <ms4851@srmist.edu.in>
2026-02-27 15:51:01 +0530

    UC3:Palindrome Check Using string reverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,15 +1,18 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        System.out.println("Palindrome Checker- Hardcore Input");
-        string word="madam";
-        string reverse="";
-        for(int i=word.length()-1;i>=0;i--){
-            reverse=reverse + word.charAt(i);
+        String input = "madam";
+
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        String reversed = "";
+
+        for (int i = normalized.length() - 1; i >= 0; i--) {
+            reversed += normalized.charAt(i);
         }
-        if(word.equals(reverse)){
-            System.out.println(word + "is a palindrome");
-        }else{
-            System.out.println(word + "is NOT a Palindrome");
+
+        if (normalized.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a palindrome");
         }
     }
 }
